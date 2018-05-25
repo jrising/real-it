8fa171ad2583869cdfed4d09ebfdead9d71f24e8
James Rising <jarising@gmail.com>
2018-05-25 18:29:47 +0100

    video watching

--- camera.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-while True:
-    cam = cv2.VideoCapture(0)
-    s, im = cam.read() # captures image
-    cv2.imshow("Test Picture", im) 
--- display.py
+++ display.py
@@ -1,23 +1,41 @@
+from Tkinter import *
 import numpy as np
 import cv2
-import Tkinter
 from PIL import Image, ImageTk
 
+tk = Tk()
+tk.title = "Real-It"
+tk.resizable(0,0)
+tk.wm_attributes("-topmost", 1)
+
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+
 cam = cv2.VideoCapture(0)
-s, img = cam.read() # captures image
 
-#Rearrang the color channel
-b,g,r = cv2.split(img)
-img = cv2.merge((r,g,b))
+class Eyeball:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.panel = Label(self.canvas)
+        self.image = None
 
-# A root window for displaying objects
-root = Tkinter.Tk()  
+    def draw(self):
+        s, img = cam.read() # captures image
 
-# Convert the Image object into a TkPhoto object
-im = Image.fromarray(img)
-imgtk = ImageTk.PhotoImage(image=im) 
+        #Rearrang the color channel
+        b,g,r = cv2.split(img)
+        img = cv2.merge((r,g,b))
 
-# Put it in the display window
-Tkinter.Label(root, image=imgtk).pack() 
+        # Convert the Image object into a TkPhoto object
+        im = Image.fromarray(img)
+        imgtk = ImageTk.PhotoImage(image=im) 
 
-root.mainloop() # Start the GUI
+        # Put it in the display window
+        self.panel.configure(image=imgtk)
+        self.image = imgtk
+        self.panel.pack()
+        self.canvas.after(1, self.draw)
+
+eyeball = Eyeball(canvas)
+eyeball.draw()
+tk.mainloop()
